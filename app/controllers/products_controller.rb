9e2b20edcffa961fc471bc08316cb1870f66ccb2
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-22 15:46:17 -0700

    Added private to ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,6 @@ class ProductsController < ApplicationController
 
   private
 
-
   # need to add merchant_id
   def product_params
     return params.require(:product).permit(:name, :description, :active, :stock_qty, :price)
