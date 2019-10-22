bf16a5f7c265ce5720d1f438eeecf6760c6b6fbe
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-22 15:45:09 -0700

    Added Products Controller and strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+
+  private
+
+
+  # need to add merchant_id
+  def product_params
+    return params.require(:product).permit(:name, :description, :active, :stock_qty, :price)
+  end
+
 end
