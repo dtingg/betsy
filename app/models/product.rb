86c4562dfb896dbb4e037843f4473798829541a6
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-22 15:49:42 -0700

    Added validations to Products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
   has_many :orderitems
   has_many :reviews
   has_and_belongs_to_many :categories
+
+  validates :merchant_id, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
 end
