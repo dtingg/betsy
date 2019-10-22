20bbb6ad0aa4bc245c583a209171fa3d897f91c3
Dianna Tingg <dtingg@gmail.com>
2019-10-22 15:49:43 -0700

    added routes, relationships, validations for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,8 @@
 class OrdersController < ApplicationController
+  
+  private
+  
+  def order_params
+    return params.require(:order).permit(:status, :name, :address, :city, :state, :zipcode, :order_date, :merchant_id)
+  end  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  belongs_to :merchant
+  has_many :orderitems
+  
+  validates :status, presence: true
+  validates :order_date, presence: true 
 end
