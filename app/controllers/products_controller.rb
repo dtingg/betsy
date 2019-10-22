94eca2a44f663fa52bb26f53d6c715d49c1496f4
Raisah <52141232+rvesteinsdottir@users.noreply.github.com>
2019-10-22 16:13:53 -0700

    Update app/controllers/products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
 
   # need to add merchant_id
   def product_params
-    return params.require(:product).permit(:name, :description, :active, :stock_qty, :price)
+    return params.require(:product).permit(:name, :description, :active, :stock_qty, :price, :merchant_id)
   end
 
 end
