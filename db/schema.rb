b6f7558366fcd10fddfe20dc87bdcdda1fafb190
Dianna Tingg <dtingg@gmail.com>
2019-10-23 12:21:29 -0700

    added orderitem controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_223745) do
+ActiveRecord::Schema.define(version: 2019_10_23_183012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2019_10_22_223745) do
     t.integer "uid"
     t.string "username"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orderitems", force: :cascade do |t|
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
