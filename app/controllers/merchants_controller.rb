a6312bce95c52531bb6bae30d390769ac007df05
KKennedyCodes <holmstedtk@gmail.com>
2019-10-23 14:03:29 -0700

    Added Controller Methods - Show and Index

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,39 +1,48 @@
 class MerchantsController < ApplicationController
   def index 
+    @merchants = Merchant.all
   end
-
+  
   def show 
+    merchant_id = params[:id]
+    @merchant = Merchant.find_by(id: merchant_id)
+    
+    if @merchant.nil?
+      flash[:error] = "Not a Valid Merchant"
+      render merchants_path
+    end
+    
   end
-
+  
   def new 
   end
-
+  
   def create
   end
-
+  
   def edit 
   end
-
+  
   def update 
   end
-
-
+  
+  
   # def index 
   #   @users = User.alphabetic
   # end
-
+  
   # def show
   #   @user = User.find_by(id: params[:id])
-    
+  
   #   if @user.nil?
   #     redirect_to root_path
   #     return 
   #   end
   # end
-
+  
   # def create
   #   auth_hash = request.env["omniauth.auth"]
-
+  
   #   user = User.find_by(uid: auth_hash[:uid], provider: "github")
   #   if user
   #     # User was found in the database
@@ -42,7 +51,7 @@ class MerchantsController < ApplicationController
   #     # User doesn't match anything in the DB
   #     # Attempt to create a new user
   #     user = User.build_from_github(auth_hash)
-
+  
   #     if user.save
   #       flash[:success] = "Logged in as new user #{user.name}"
   #     else
@@ -55,20 +64,20 @@ class MerchantsController < ApplicationController
   #       return redirect_to root_path
   #     end
   #   end
-
+  
   #   # If we get here, we have a valid user instance
   #   session[:user_id] = user.id
   #   return redirect_to root_path
   # end
-
+  
   # def destroy
   #   session[:user_id] = nil
   #   flash[:success] = "Successfully logged out!"
-
+  
   #   redirect_to root_path
   # end
-
-
+  
+  
   # def current
   #   @current_user = User.find_by(id: session[:user_id])
   #   unless @current_user
