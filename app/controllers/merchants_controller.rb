78587c011b21b46fa80785a78f21026f6d3902b8
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-23 11:35:28 -0700

    Merchants Controller and Test buildout. Added test_helper.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,4 +1,23 @@
 class MerchantsController < ApplicationController
+  def index 
+  end
+
+  def show 
+  end
+
+  def new 
+  end
+
+  def create
+  end
+
+  def edit 
+  end
+
+  def update 
+  end
+
+
   # def index 
   #   @users = User.alphabetic
   # end
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 describe MerchantsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "should display all merchants" do
+      get merchants_path 
+      must_respond_with :success
+    end
+
+    it "should not break if there are no merchants" do 
+      Merchant.all.destroy_all
+      get merchants_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+
+    it "should respond with success when given a valid merchant" do
+      merchant_one = merchants(:merchant_one)
+
+      get merchant_path(merchant_one.id)
+      must_respond_with :success
+    end
+
+    it "should respond with redirect with an invalid merchant" do 
+      get merchant_path(-1)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "new" do
+  end
+
+  describe "create" do
+  end
+
+  describe "edit" do
+  end
+
+  describe "update" do 
+  end
+
+
 end
