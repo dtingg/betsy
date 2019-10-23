535d70b85e9f266ab96db651ae5ef852d8966fe4
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-23 13:52:29 -0700

    Added index and show for Products controller and tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,37 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      redirect_to products_path
+      return
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   private
 
   # need to add merchant_id
   def product_params
     return params.require(:product).permit(:name, :description, :active, :stock_qty, :price, :merchant_id)
   end
-
+  
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,63 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  before do
+    merchant = Merchant.create(uid: 9999, username: "random", email: "email")
+    @product = Product.create(name: "Test ", price: 10.00, merchant: merchant)
+  end
+
+  describe "index" do
+    it "responds with success when there are products" do
+
+      get products_path
+
+      must_respond_with :success
+    end
+
+    it "responds with success when there are no products" do
+      @product.destroy
+
+      get products_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "redirects to the details page of a valid product" do
+
+      valid_product_id = @product.id
+
+      get product_path(valid_product_id)
+
+      must_respond_with :success
+    end
+
+    it "redirects to products for invalid product" do
+      invalid_product_id = -1
+
+      get product_path(invalid_product_id)
+
+      must_respond_with :redirect
+      must_redirect_to products_path
+    end
+  end
+  
+  describe "new" do
+
+
+  end
+
+  describe "create" do
+  end
+
+  describe "edit" do
+  end
+
+  describe "update" do
+  end
+
+  describe "destroy" do
+  end
 end
