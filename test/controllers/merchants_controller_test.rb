ac740c1624e07872ca14e86696ea130d0487d06f
KKennedyCodes <holmstedtk@gmail.com>
2019-10-23 13:46:19 -0700

    added create method test

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -6,40 +6,63 @@ describe MerchantsController do
       get merchants_path 
       must_respond_with :success
     end
-
+    
     it "should not break if there are no merchants" do 
       Merchant.all.destroy_all
       get merchants_path
       must_respond_with :success
     end
   end
-
+  
   describe "show" do
-
+    
     it "should respond with success when given a valid merchant" do
       merchant_one = merchants(:merchant_one)
-
+      
       get merchant_path(merchant_one.id)
       must_respond_with :success
     end
-
+    
     it "should respond with redirect with an invalid merchant" do 
       get merchant_path(-1)
       must_respond_with :not_found
     end
   end
-
+  
   describe "new" do
+    it "can display the new merchant page" do
+      get new_merchant_path
+      must_respond_with :success
+    end
   end
-
+  
   describe "create" do
+    it "can create a new merchant" do
+      new_merchant_params = {
+        merchant: {
+          uid: 567
+          username: "Mr. Smith"
+          email: "smith@adadev.org"
+        }
+      }
+      
+      expect {
+        post merchants_path, params: new_merchant_params 
+      }.must_change "Merchant.count", 1
+    end
+    
+    new_merchant = Merchant.find_by(uid: new_merchant_params[:merchant][:uid])
+    expect(new_merchant.username).must_equal new_merchant_params[:merchant][:username]
+    
+    must_respond_with :redirect
+    must_redirect_to merchant_path(new_merchant)
   end
-
+  
   describe "edit" do
   end
-
+  
   describe "update" do 
   end
-
-
+  
+  
 end
