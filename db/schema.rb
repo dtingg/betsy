ff2786afd9fd6d15877ec65d8f50e769f2226206
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-24 14:38:41 -0700

    Updates to Order model, Product model, OrderItem model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_202053) do
+ActiveRecord::Schema.define(version: 2019_10_24_213728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,6 @@ ActiveRecord::Schema.define(version: 2019_10_24_202053) do
     t.string "cc_exp"
     t.string "cc_cvv"
     t.datetime "order_date"
-    t.bigint "merchant_id"
-    t.index ["merchant_id"], name: "index_orders_on_merchant_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -57,7 +55,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_202053) do
     t.string "description"
     t.boolean "active", default: true
     t.integer "stock_qty", default: 10
-    t.float "price"
+    t.decimal "price", precision: 5, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
@@ -78,7 +76,6 @@ ActiveRecord::Schema.define(version: 2019_10_24_202053) do
 
   add_foreign_key "orderitems", "orders"
   add_foreign_key "orderitems", "products"
-  add_foreign_key "orders", "merchants"
   add_foreign_key "products", "merchants"
   add_foreign_key "reviews", "products"
 end
