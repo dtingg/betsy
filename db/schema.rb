15d984922d05e2dde88c2b22f078a4e9a14593b2
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-25 16:47:44 -0700

    CategoryController, Categories Table, Categories Table Migration adding Name, String.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_213728) do
+ActiveRecord::Schema.define(version: 2019_10_25_234507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.integer "uid"
