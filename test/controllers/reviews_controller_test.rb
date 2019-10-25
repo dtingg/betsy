14f4d7ab1fc0503085e25114e40272d9ddf10725
daniela sanchez <dnsanche@gmail.com>
2019-10-24 19:14:02 -0700

    Finished tests for create on reviewscontroller

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -28,7 +28,6 @@ describe ReviewsController do
 
   describe "create" do 
     it "can create a new review" do 
-
       review_hash = {
         review:{
           product_id: new_product.id, 
@@ -41,11 +40,23 @@ describe ReviewsController do
       }.must_differ "Review.count", 1
       
       must_respond_with :redirect
-      must_redirect_to  product_path(id: new_product.id)
-      
+      must_redirect_to  product_path(id: new_product.id)     
     end
     
     it "will will not create a new product if invalid fields are given" do 
+      review_hash = {
+        review:{
+          product_id: new_product.id, 
+          rating: nil,
+        }
+      }
+
+      expect { 
+        post product_reviews_path(new_product.id), params: review_hash
+      }.wont_change "Review.count"
+      
+      expect(flash[:failure]).wont_be_nil
+      must_respond_with :bad_request 
     end
   end
   
