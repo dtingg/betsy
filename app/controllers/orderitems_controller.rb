bad925a116adea61b7726553630b90d77d6f5d41
Dianna Tingg <dtingg@gmail.com>
2019-10-24 21:24:51 -0700

    added tests for orderitem create

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -2,18 +2,47 @@ class OrderitemsController < ApplicationController
   before_action :find_orderitem, only: [:destroy]
   
   def create
-    Orderitem.create(quantity: 1, order_id: session[:order_id], product_id: 6)
+    # Orderitem.create(order_id: @cart.id, product_id: 7, quantity: 2)
+    if params[:orderitem].nil?
+      redirect_back(fallback_location: root_path)
+      return
+    end
+    
+    @orderitem = Orderitem.new(orderitem_params)
+    
+    if @orderitem.save
+      flash[:success] = "Item added to your cart"
+      redirect_back(fallback_location: root_path)
+      return
+    else
+      flash[:error] = "Error adding product to your cart"
+      redirect_back(fallback_location: root_path)
+      return
+    end
   end
   
-  def destroy
-    @orderitem.destroy
-    
-    redirect_back(fallback_location: root_path)
-  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # def destroy
+  #   @orderitem.destroy
+  
+  #   redirect_back(fallback_location: root_path)
+  # end
   
   private
   
   def find_orderitem
     @orderitem = Orderitem.find_by(id: params[:id])
+  end
+  
+  def orderitem_params
+    return params.require(:orderitem).permit(:quantity, :order_id, :product_id, :complete)
   end
 end
--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 describe OrderitemsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "create" do
+    it "does not create an orderitem if the form data is nil, and responds with redirect" do
+      orderitem_hash = { orderitem: nil }
+      
+      expect { post orderitems_path, params: orderitem_hash }.wont_change "Orderitem.count"
+      
+      must_respond_with :redirect
+    end
+    
+    it "can create a new orderitem with valid information accurately, and redirect" do
+      test_order = Order.create(status: "pending")
+      test_merchant = Merchant.create
+      test_product = Product.create(merchant_id: test_merchant.id, name: "Oatmeal soap", price: 5.55)
+      
+      orderitem_hash = { orderitem: { order_id: test_order.id, product_id: test_product.id, quantity: 3 }}
+      
+      expect { post orderitems_path, params: orderitem_hash }.must_change "Orderitem.count", 1
+      
+      new_orderitem = Orderitem.last
+      
+      expect(new_orderitem.order_id).must_equal orderitem_hash[:orderitem][:order_id]
+      expect(new_orderitem.product_id).must_equal orderitem_hash[:orderitem][:product_id]
+      expect(new_orderitem.quantity).must_equal orderitem_hash[:orderitem][:quantity]
+      expect(flash[:success]).must_equal "Item added to your cart"
+      must_respond_with :redirect  
+    end
+    
+    it "will not create a new orderitem if a required field is missing" do
+      test_order = Order.create(status: "pending")
+      test_merchant = Merchant.create
+      test_product = Product.create(merchant_id: test_merchant.id, name: "Oatmeal soap", price: 5.55)
+      
+      orderitem_hash = { orderitem: { order_id: test_order.id, product_id: test_product.id, quantity: nil }}
+      
+      expect { post orderitems_path, params: orderitem_hash }.wont_change "Orderitem.count", 1
+      
+      expect(flash[:error]).must_equal "Error adding product to your cart"
+      must_respond_with :redirect
+    end      
+  end
 end
