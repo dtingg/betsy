b5d402f82562e3ffff915686342f2b2bf8f9b583
KKennedyCodes <holmstedtk@gmail.com>
2019-10-25 23:50:35 -0700

    Added Total Active Products Method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,30 @@
 class Merchant < ApplicationRecord
   has_many :products, dependent: :destroy
   
+  def member_since
+    time_diff_days = ((Time.current - self.created_at) / 1.day).round
+    if time_diff_days == 0
+      time_diff_hours  = ((Time.current - self.created_at) / 1.hour).round
+      membership_duration = time_diff_hours.to_s + " hours"
+      return membership_duration
+    end
+    if time_diff_days >= 365
+      time_diff_hours  = ((Time.current - self.created_at) / 1.year).round
+      membership_duration = time_diff_hours.to_s + " years"
+      return membership_duration
+    end
+    membership_duration = time_diff_hours.to_s + " days"
+    return membership_duration
+  end
+  
+  def active_products
+    active_prod = self.products.where(active: true)
+    if active_prod.count == 1
+      return active_prod.count.to_s + " Active Product"
+    end
+    return active_prod.count.to_s + " Active Products"
+  end
+  
   def self.build_from_github(auth_hash)
     merchant = Merchant.new
     merchant.uid = auth_hash[:uid]
@@ -13,6 +37,7 @@ class Merchant < ApplicationRecord
   def self.alphabetic
     return Merchant.order(name: :asc)
   end
+  
   
   def self.gross_sales
   end
