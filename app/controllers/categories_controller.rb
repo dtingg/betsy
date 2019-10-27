188256beef36a640b07a2686fb836f032b4bc79c
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-27 14:06:45 -0700

    Show and Index CategoriesController tests.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,23 @@
 class CategoriesController < ApplicationController
+  
+  before_action :valid_category?, only: [:show]
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
 
   private
   def category_params
    
     return params.require(:category).permit(:name, product_ids: [])
+  end
+
+  def valid_category?
+    @category = Category.find_by(id: params[:id])
+    if @category.nil? 
+      flash[:error] = "category doesn't exist"
+      redirect_to categories_path
+    end
   end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "should respond with success" do
+    get categories_path
+    must_respond_with :success
+    end
+
+    it "should not break if there are no categories" do
+      Category.destroy_all
+      get categories_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "should respond with success when asked to show a particular category" do
+      bubbly_category = categories(:bubbly)
+
+      get category_path(bubbly_category.id)
+      must_respond_with :success
+    end
+
+    it "should respond with redirect when given an invalid category" do
+      get category_path(-1)
+      must_respond_with :redirect
+    end
+  end
 end
