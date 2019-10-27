fe509f79d8ce1c2503c2fea0e91794ebe5d973bd
Dianna Tingg <dtingg@gmail.com>
2019-10-27 13:38:02 -0700

    added custom validation messages to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,13 @@ class Order < ApplicationRecord
   has_many :orderitems, dependent: :destroy
   
   validates :status, presence: true
-  validates :name, presence: true, :on => :update
-  validates :email, presence: true, :on => :update
-  validates :address, presence: true, :on => :update
-  validates :city, presence: true, :on => :update
-  validates :state, presence: true, :on => :update
-  validates :zipcode, presence: true, :on => :update
-  validates :cc_num, presence: true, :on => :update
-  validates :cc_exp, presence: true, :on => :update
-  validates :cc_cvv, presence: true, :on => :update  
+  validates :name, presence: { message: "Name can't be blank"}, :on => :update
+  validates :email, presence: { message: "E-mail can't be blank"}, :on => :update
+  validates :address, presence: { message: "Address can't be blank"}, :on => :update
+  validates :city, presence: { message: "City can't be blank"}, :on => :update
+  validates :state, presence: { message: "State can't be blank"}, :on => :update
+  validates :zipcode, presence: { message: "Zip code can't be blank"}, :on => :update
+  validates :cc_num, length: { in: 13..16, message: "Credit card number must be 13-16 numbers in length" }, :on => :update
+  validates :cc_exp, presence: { message: "Credit card expiration can't be blank"}, :on => :update
+  validates :cc_cvv, length: { in: 3..4, message: "Credit card cvv must be 3-4 numbers in length" }, :on => :update  
 end
