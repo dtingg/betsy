f9b1967305ef3e2ec174818b6fe999caa0778993
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-27 12:57:08 -0700

    categories_products join table, strong params updated for CategoriesController and ProductsController, many-to-many relation established in Category and Product models.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
 class CategoriesController < ApplicationController
 
-
+  private
+  def category_params
+   
+    return params.require(:category).permit(:name, product_ids: [])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :merchant
   has_many :orderitems, dependent: :destroy
   has_many :reviews, dependent: :destroy  
-  #has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories
   
   validates :merchant_id, presence: true
   validates :name, presence: true, uniqueness: true
