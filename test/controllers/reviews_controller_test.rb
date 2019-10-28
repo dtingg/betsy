c681ffda06fd8ce5ab20a2d8a71ce040e27f53c5
daniela sanchez <dnsanche@gmail.com>
2019-10-28 16:23:17 -0700

    Finished reviews controller tests with authentication

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,20 +1,9 @@
 require "test_helper"
 
-describe ReviewsController do
-  let (:new_merchant) { Merchant.create(uid: 9999, username: "random", email: "email") }
-  let (:new_product) { Product.create(name: "Test ", price: 10.00, merchant: new_merchant) }
-  let (:new_review) { Review.new(product_id: new_product.id, rating: 5) }
-  
+describe ReviewsController do 
+  let (:new_product) { products(:potter) }
+
   describe "guest user (not-authenticated)" do
-    before do
-      product = products(:potter)
-        review_hash = {
-          review:{
-            product_id: product.id, 
-            rating: 5,
-          }
-        }
-    end
     describe "new" do
       it 'shows the form to add a review' do
         get new_product_review_path(new_product.id)
@@ -22,30 +11,30 @@ describe ReviewsController do
       end
     end
 
-    describe "create" do 
-      
-      it "can create a new review" do 
-        product = products(:potter)
+    describe "create" do   
+      it "can create a review" do 
         review_hash = {
           review:{
-            product_id: product.id, 
+            product_id: new_product.id,
             rating: 5,
+            comment: "Great!"
           }
         }
   
         expect { 
-          post product_reviews_path(product.id), params: review_hash
+          post product_reviews_path(new_product.id), params: review_hash
         }.must_differ "Review.count", 1
         
         must_respond_with :redirect
-        must_redirect_to  product_path(id: product.id)     
+        must_redirect_to  product_path(id: new_product.id)     
       end
       
-      it "will will not create a new product if invalid fields are given" do 
+      it "will not create a new review if invalid fields are given" do 
         review_hash = {
           review:{
             product_id: new_product.id, 
             rating: nil,
+            comment: nil
           }
         }
   
@@ -57,26 +46,90 @@ describe ReviewsController do
         must_respond_with :bad_request 
       end
     end
-  
   end
   
-  
   describe "authentication" do
+    
     before do
+      @user = merchants(:merchant_three)
+      @merchant_product = Product.create(merchant_id: @user.id, name: "Soap", price: 10)
+      @other_merchant_product = products(:rose) 
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(@user))
+      get auth_github_callback_path
     end
-    # perform login 
-    describe "create restrictions for merchants" do
-      it "should not allow a merchant to review their own product" do
-        # needs a test
+
+    describe "new" do
+      it "will not show the form to add a review when his product" do     
+        review_hash = {
+          review:{
+            product_id: @merchant_product.id,
+            rating: 5,
+            comment: "Great!"
+          }
+        }
+    
+        get new_product_review_path(@merchant_product.id)
+        
+        must_redirect_to  product_path(id: @merchant_product.id) 
+        expect(flash[:failure]).wont_be_nil
       end
 
-      it "should allow a merchant to review any product that is not theirs" do
-        # needs a test
+      it "will show the form to add a review when not his product" do
+        
+        review_hash = {
+          review:{
+            product_id: @other_merchant_product.id,
+            rating: 5,
+            comment: "Great!"
+          }
+        }
+        
+        get new_product_review_path(@other_merchant_product.id)
+    
+        must_respond_with :success 
+      end
+    end
+
+    describe "create" do
+      it "will not create a review when his own product" do 
+        review_hash = {
+          review:{
+            product_id: @merchant_product.id,
+            rating: 5,
+            comment: "Great!"
+          }
+        }
+  
+        expect { 
+          post product_reviews_path(@merchant_product.id), params: review_hash
+        }.wont_change "Review.count"
+
+        must_redirect_to  product_path(id: @merchant_product.id) 
+        expect(flash[:failure]).wont_be_nil  
+      end
+
+      it "will create a review when other merchant product" do 
+        review_hash = {
+          review:{
+            product_id: @other_merchant_product.id,
+            rating: 5,
+            comment: "Great!"
+          }
+        }
+  
+        expect { 
+          post product_reviews_path(@other_merchant_product.id), params: review_hash
+        }.must_differ "Review.count", 1
+
+        must_redirect_to  product_path(id: @other_merchant_product.id) 
+        expect(flash[:success]).wont_be_nil  
       end
     end
   end
 end
+ 
 
-
+   
   
   
