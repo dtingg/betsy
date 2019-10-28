b504fd7c6d9fd313139dd60f2a2aacf24ca68f2b
KKennedyCodes <holmstedtk@gmail.com>
2019-10-27 23:02:52 -0700

    Added CSS for Shopping Cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ApplicationRecord
   has_many :orderitems, dependent: :destroy
   
   validates :status, presence: true
+  
+  def order_item_total
+  end
+  
+  def order_item_total
+  end
+  
+  def order_item_qty
+  end
 end
