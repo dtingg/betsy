8e605660610380b99ffd4eab9b57e73d9b1b841b
Dianna Tingg <dtingg@gmail.com>
2019-10-29 11:36:44 -0700

    updated order edit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,17 +14,14 @@ class OrdersController < ApplicationController
     end
   end
   
-  
-  
-  
-  
-  
-  
-  
   def edit
-    if @cart.nil?
+    if @cart.nil? #@order.nil?
       redirect_back(fallback_location: root_path)
       return
+    end
+    
+    if @cart.status == "complete" #@order?
+      redirect_to order_path(@order)
     end
   end
   
@@ -37,13 +34,29 @@ class OrdersController < ApplicationController
     if @cart.update(order_params)
       flash[:success] = "Thank you for your order!"  
       redirect_to order_path(@cart)
-      session[:order_id] = nil
+      session[:cart_id] = nil
       return
     else
       render :edit
       return
     end
   end
+  
+  #   def update
+  #     if @order.nil?
+  #       redirect_back(fallback_location: root_path)
+  #       return
+  #     end
+  #     if @order.update(order_params)
+  #       flash[:success] = "Thank you for your order!"  
+  #       redirect_to order_path(@order)
+  #       session[:cart_id] = nil
+  #       return
+  #     else
+  #       render :edit
+  #       return
+  #     end
+  #   end
   
   def cart
   end
@@ -58,49 +71,3 @@ class OrdersController < ApplicationController
     return params.require(:order).permit(:status, :name, :email, :address, :city, :state, :zipcode, :cc_num, :cc_exp, :cc_cvv, :order_date, :merchant_id)
   end  
 end
-
-
-
-####
-
-
-#   def edit
-#     if @order.nil?
-#       redirect_back(fallback_location: root_path)
-#       return
-#     end
-
-#     if @order.status == "complete"
-#       redirect_to order_path(@order)
-#     end
-#   end
-
-#   def update
-#     if @order.nil?
-#       redirect_back(fallback_location: root_path)
-#       return
-#     end
-#     if @order.update(order_params)
-#       flash[:success] = "Thank you for your order!"  
-#       redirect_to order_path(@order)
-#       session[:cart_id] = nil
-#       return
-#     else
-#       render :edit
-#       return
-#     end
-#   end
-
-#   def cart
-#   end
-
-#   private
-
-#   def find_order
-#     @order = Order.find_by(id: params[:id])
-#   end
-
-#   def order_params
-#     return params.require(:order).permit(:status, :name, :email, :address, :city, :state, :zipcode, :cc_num, :cc_exp, :cc_cvv, :order_date, :merchant_id)
-#   end  
-# end
