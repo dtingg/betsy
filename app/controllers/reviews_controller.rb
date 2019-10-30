2687eb1509e81d571f7a46915034888d7eb441a0
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-29 21:58:36 -0700

    Small changes to Reviews Controller methods and tests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,20 @@
 class ReviewsController < ApplicationController
-  before_action :user_validation, only: [:new, :create]
+  before_action :validate_merchant
 
   def new
     @review = Review.new()
   end
 
   def create
-   @review = Review.new(review_params)
+    @review = Review.new(review_params)
     if @review.save
       flash[:success] = "Review was succesfully posted"
+      
       redirect_to product_path(id: @review.product_id)
       return
     else 
       flash[:failure] = "A problem occurred"
+
       render :new, status: :bad_request 
       return
     end  
@@ -24,12 +26,16 @@ class ReviewsController < ApplicationController
     return params.require(:review).permit(:comment, :rating, :reviewer, :product_id, :date)
   end
 
-  def user_validation
-    if session[:user_id] != nil
-      @product = Product.find_by(id: params[:product_id])    
-      if @product.merchant_id == Merchant.find_by(id: session[:user_id]).id
+  def validate_merchant
+    if session[:user_id]
+      @product = Product.find_by(id: params[:product_id]) 
+      merchant = Merchant.find_by(id: session[:user_id])
+
+      if @product.merchant == merchant
         flash[:failure] = "A problem occurred: You cannot review your own product."
+
         redirect_to product_path(id: @product.id) 
+        return 
       end
     end 
   end 
