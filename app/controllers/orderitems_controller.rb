61b3e9a9132352a2d901cd95f2ca3bac4746ce0d
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-11-01 11:36:42 -0700

    Orderitem cancelled adds back to stock, Dashboard view fixes

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -56,13 +56,16 @@ class OrderitemsController < ApplicationController
     if @orderitem.nil?
       redirect_back(fallback_location: root_path)
       return
-      #   # This is for updating quantity in shopping cart view
-      # elsif @orderitem.mark_complete
-      #   @orderitem.mark_complete
-      
     elsif @orderitem.update(orderitem_params)
       if @orderitem.complete == true
+        @orderitem.mark_complete
         flash[:success] = "Product Order Completed"
+        redirect_back(fallback_location: root_path)
+        return  
+      end
+      if orderitem_params[:complete] == nil
+        @orderitem.mark_cancelled
+        flash[:success] = "Orderitem cancelled"
         redirect_back(fallback_location: root_path)
         return  
       end
