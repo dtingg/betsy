dd69fc42158f1f6bcdb1786fa1cee1b653812b14
Dianna Tingg <dtingg@gmail.com>
2019-11-01 15:53:39 -0700

    fixed bug for cancel

--- config/routes.rb
+++ config/routes.rb
@@ -7,22 +7,23 @@ Rails.application.routes.draw do
   get "/cart", to: "orders#cart", as: "cart"
   get "/checkout/:id", to: "orders#edit", as: "checkout"
   resources :orders, except: [:index, :new, :create, :put, :delete]
-
+  
   resources :merchants, only: [:index, :show]
   post "/logout", to: "merchants#logout", as: "logout"
   get "/dashboard", to: "merchants#dashboard", as: "dashboard"
   
   resources :orderitems, except: [:index, :put, :new, :show]
-
+  patch "/orderitems/:id/cancel/", to: "orderitems#cancel", as: "cancel"
+  
   resources :products, except: [:put]
   resources :products, only: [:show] do
     resources :reviews, only: [:new, :create]
   end
-
+  
   resources :reviews, only: [:new, :create]
-
+  
   resources :categories, only: [:show, :index, :new, :create]
-
+  
   get "/auth/github", as: "github_login"
   get "/auth/github/callback", to: "merchants#create", as: "auth_callback"
 end
